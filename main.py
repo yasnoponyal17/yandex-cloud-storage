fbedaeb027401b2c8c2b3ea54b37328b502c5232
Sergei Efimov <snootfornoyeah@yandex.ru>
2026-04-07 15:22:19 +0300

    W_W

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
 import boto3
-s3 = boto3.client('s3')
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+s3 = boto3.client(
+	's3',
+	endpoint_url='https://storage.yandexcloud.net',
+	aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+  aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+	)
 
 # Сохранение файла
-s3.put_object(Bucket='compworkshop', Key='example.txt', Body="Hello, World!")
-s3.put_object(Bucket='compworkshop', Key='temp.txt', Body='This file will be delete')
+def upload_file(bucket, key, body):
+	s3.put_object(Bucket=bucket, Key=key, Body=body)
+	print(f"Файл {key} успешно создан.")
 
 # Удаление файла
-s3.delete_object(Bucket='compworkshop', Key="temp.txt")
+def delete_file(bucket, key):
+	s3.delete_object(Bucket=bucket, Key=key)
+	print(f'Файл {key} успешно удален')
 
-# Получение данных из бакета
-for key in s3.list_objects(Bucket='compworkshop')['Contents']:
-	print(key['Key'])
+# Получение содержимое бакета
+def get_files(bucket):
+	print("Содержимое бакета:")
+	for key in s3.list_objects(Bucket=bucket)['Contents']:
+		print(key['Key'])
 
-# Получить содержимое файла
-object = s3.get_object(Bucket='compworkshop', Key='example.txt')
-print(object['Body'].read())
+# Получение содержимого файла
+def get_file(bucket, key):
+	object = s3.get_object(Bucket=bucket, Key=key)
+	print(f'Содержимое файла {key}')
+	print(object['Body'].read())
+
+if __name__ == "__main__":
+	upload_file('compworkshop', 'example.txt', 'Hello, world!')
+	upload_file('compworkshop', 'temp.txt', 'This file will be delete')
+
+	get_files('compworkshop')
+
+	get_file('compworkshop', 'example.txt')
+
+	delete_file('compworkshop', 'temp.txt')
+
+	# Содержимое бакета после удаления temp.txt
+	get_files('compworkshop')
